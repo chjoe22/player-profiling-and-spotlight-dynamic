9c4672edf311c7085af93227ac4ebfe4fdc09301
Christian <chjoe@student.sdu.dk>
2026-04-07 23:49:21 +0200

    minor change

--- script/pipeline/video_pipeline.py
+++ script/pipeline/video_pipeline.py
@@ -9,7 +9,7 @@ import numpy as np
 import csv
 import torch
 import gc
-from tqdm import tqdm # Library for process bar
+from tqdm import tqdm # Library for process-bar
 import warnings
 import datetime
 
