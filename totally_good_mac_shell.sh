f5d761111529c648f256d82e746263e253f124e9
Christian <chjoe22@student.sdu.dk>
2026-04-10 14:20:06 +0200

    added mac support for shell script

---
+++ totally_good_mac_shell.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+cd "$(dirname "$0")"
+
+echo "Starting Pipeline on macOS..."
+
+echo "Running Speaker Metrics..."
+pushd script/engagement_metrix > /dev/null
+python3 speaker_time.py
+python3 speaker_time_analyzer.py
+python3 speaker_stat_plotter_seaborn.py
+popd > /dev/null
+
+echo "Running Context..."
+pushd script/context > /dev/null
+python3 context_extraction.py
+python3 combat.py
+popd > /dev/null
+
+echo "Running Combination..."
+pushd script/combination > /dev/null
+python3 emotion_combination.py
+popd > /dev/null
+
+echo "Running Profiling..."
+pushd script/profiling > /dev/null
+python3 profile_generator.py
+popd > /dev/null
+
+echo "Successfully ran all scripts"
